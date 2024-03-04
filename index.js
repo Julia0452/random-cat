445c08f9ff12de22c6d9bb548a21bdf58813d3a8
Juan Fretes <juanfretesg@gmail.com>
2024-03-04 18:38:54 -0300

    funcionalidad del spinner

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var imagen = document.getElementById("imagen")
 var boton = document.getElementById("button")
+var spinner = document.getElementById("spinner")
 
 function numeroAleatorio() {
     var numero = Math.floor(Math.random() * 99) + 1
@@ -12,9 +13,19 @@ function crearLink() {
     return link
 }
 
+function mostrarSpinner() {
+    spinner.style.display = "block"
+}
+
+function ocultarSpinner() {
+    spinner.style.display = "none"
+}
+
 function cambiarImagen() {
     var link = crearLink()
     imagen.src = link
+    mostrarSpinner()
 }
 boton.onclick = cambiarImagen
 imagen.onerror = cambiarImagen
+imagen.onload = ocultarSpinner
